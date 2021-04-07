fe84c19778509aab89450f307a9bebbd0eebe97c
邹家进 <1273614957@qq.com>
2021-04-07 22:22:11 +0800

    4.7

---
+++ pages/basics/success.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<view class="tips">
+			<p class="cuIcon-roundcheckfill" style="font-size: 200px;color: #39B54A;"></p>
+			<p style="font-size: 20px;color: #39B54A;">订单已生成，正在通知回收员接单上门，请耐心等待！</p>
+			<p @click="back()" style="font-size: 16px;color: #0da0ef;margin-top: 200px;text-decoration: underline;font-weight: 700;">返回首页>></p>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			back(){
+				uni.reLaunch({
+					url: '/pages/index/index',
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		}
+	}
+	
+</script>
+
+<style scoped>
+	.tips{
+		position: absolute;
+		top: calc(50% - 50vw);
+		text-align: center;
+		width: 100%;
+		padding: auto 30px;
+	}
+</style>
