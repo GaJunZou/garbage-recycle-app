96b709501c526a29017576a7c43c6ae4e636ebcb
邹家进 <1273614957@qq.com>
2021-04-23 09:26:12 +0800

    4.23

--- main.js
+++ main.js
@@ -100,9 +100,9 @@ Vue.prototype.send = function(channel,message,body){
 	});
 } 
 
-Vue.prototype.base = 'http://172.20.10.7:3000';
-Vue.prototype.socket = 'ws://172.20.10.7:3000';
-Vue.prototype.uploadImg = "http://172.20.10.7:8002/aliyun-service/upload-image";
+Vue.prototype.base = 'http://192.168.191.1:3000';
+Vue.prototype.socket = 'ws://192.168.191.1:3000';
+Vue.prototype.uploadImg = "http://192.168.191.1:8002/aliyun-service/upload-image";
 
 String.prototype.time = function() {
 	let time = this.split(' ')[1].split(':');
